e04cc10379f4a4525065b93ea24f5293440255a3
Lapecru64 <perlura64@outlook.com>
2023-08-02 19:04:10 -0600

    E-COMMERCE Parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage'
+import { useEffect } from 'react'
+import { getAllProductsThunk } from './store/slices/products.slice'
+import { useDispatch } from 'react-redux'
+import Header from './components/shared/Header'
 
 function App() {
+
+  const dispatch = useDispatch()
   
+  useEffect(() => {
+  dispatch(getAllProductsThunk())
+  }, [])
 
   return (
-    <>
-      <h1>Entregable-6</h1>
-    </>
+    <div>
+      <Header />
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      </Routes>
+      
+      </div>
   )
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux"
+import CardProduct from "../components/HomePage/CardProduct"
+
+const HomePage = () => {
+
+const products = useSelector(reducer => reducer.products)
+
+console.log(products)
+
+  return (
+    <div>
+      <div>
+        {
+         products?.map(prod => (
+          <CardProduct 
+          key={prod.id}
+          product={prod}
+          />
+         ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
